2672f42296186a412c0c399007603b3b26540d54
Joel <jperezcalvo@danielcastelao.org>
2023-03-02 17:37:49 +0100

    update para descargar playlist con main.py

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from pytube import YouTube
+from pytube import Playlist
+
+p = Playlist('https://www.youtube.com/watch?v=LKQc7deraKo&list=PLhlzPAwSE2kIExse-BctOdM2XnrJTGEo7')
+
+for video in p.videos:
+    video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+print('funciona')
+
+
+
 
 #link = input("Enter the link: ")
-yt = YouTube("https://www.youtube.com/watch?v=aFqTjk3kcEw&ab_channel=Garajedeideas")
+#yt = YouTube("https://www.youtube.com/watch?v=aFqTjk3kcEw&ab_channel=Garajedeideas")
+
 
 #Title of video
-print("Title: ",yt.title)
+#print("Title: ",yt.title)
 #Number of views of video
-print("Number of views: ",yt.views)
+#print("Number of views: ",yt.views)
 #Length of the video
-print("Length of video: ",yt.length,"seconds")
+#print("Length of video: ",yt.length,"seconds")
 #Description of video
-print("Description: ",yt.description)
+#print("Description: ",yt.description)
 #Rating
-print("Ratings: ",yt.rating)
+#print("Ratings: ",yt.rating)
 
 
-yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+#yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
